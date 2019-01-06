a8ec5e5690d5b7b46f25019d3d873b2869d975e5
EC2 Default User <ec2-user@ip-172-31-37-136.us-east-2.compute.internal>
2019-01-06 18:26:05 +0000

    Adding changes a new images

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'redy_wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'R00t13#');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
